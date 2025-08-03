cc1053e70cdf59e45f4069249ea7dd5f6b85a1c3
fedelmar <fsuarez91@gmail.com>
2025-08-03 12:57:17 -0300

    initial comint

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe(); 
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public view {
+
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.getOwner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsCorrect() public view {
+        console.log("fundMe.getVersion()", fundMe.getVersion());
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+    
+}
